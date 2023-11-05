109adda1781c4eb20cf86d828008db64d7bbd39a
Ken Schutte <kenschutte@gmail.com>
2023-11-05 14:01:16 +0530

    added simple tests

---
+++ tests/test_basic.py
@@ -0,0 +1,26 @@
+"""
+Usage:
+- pip install pytest
+- `pytest tests`
+  - from repo root
+
+"""
+
+def test_wrapper():
+    """
+    Run "1 2 add =" via GSWrapper and ensure we get '3'
+    """
+    from IPostScript.kernel import GSWrapper
+    gs = GSWrapper()
+
+    print(">>>", repr(gs.run("1 2 add =")))
+
+    
+def test_platform():
+    """
+    test should work anywhere, so we just check
+    that we can call is_windows() and get bool.
+    """
+    from IPostScript.kernel import is_windows
+    assert(is_windows() in (True,False))
+    
